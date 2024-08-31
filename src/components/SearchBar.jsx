9a165c1e4ae786ab6dc0b7d0f50e24209b29d2a4
DamodarPatil <patildamodar1983@gmail.com>
2024-08-31 17:32:57 +0530

    Add search functionality to SearchBar component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,34 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { fetchRecipes } from "../features/recipesSlice";
 
 const SearchBar = () => {
-  return (
-    <div>SearchBar</div>
-  )
-}
+  const [query, setQuery] = useState("");
+  const dispatch = useDispatch();
 
-export default SearchBar
+  const handleSearch = () => {
+    if (query) {
+      dispatch(fetchRecipes);
+    }
+  };
+
+  return (
+    <div className="flex items-center p-4 bg-gray-200">
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Search for recipes...."
+        className="p-2 border border-gray-300 rounded"
+      />
+      <button
+        onClick={handleSearch}
+        className="ml-2 p-1 px-2 bg-blue-500 text-white rounded"
+      >
+        Search
+      </button>
+    </div>
+  );
+};
+
+export default SearchBar;
